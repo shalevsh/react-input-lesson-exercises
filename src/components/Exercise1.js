f5d0c09d2d75d9d70aa266e66f65b40b304abd51
shalev <shalevsharf@gmail.com>
2022-11-08 16:25:48 +0200

    ex1

--- src/components/Exercise1.js
+++ src/components/Exercise1.js
@@ -1,21 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Exercise1 extends Component {
-
     constructor() {
-        super()
+        super();
         this.state = {
             name: "",
-            age: ""
-        }
+            age: "",
+        };
     }
+    updateName = (event) => {
+        this.setState({ name: event.target.value });
+    };
 
+    updateAge = (event) => {
+        this.setState({ age: event.target.value });
+    };
+    alertText = () => {
+        alert(
+            `come in ${this.state.name} you’re ${this.state.age} - that’s good enough`
+        );
+    };
+    //Come in Haliax, you’re 40 - that’s good enough
     render() {
         return (
             <div>
-                <input id="name-input" />
-                <input id="age-input" />
-                <button>Go to Bar</button>
+                <input id="name-input" onChange={this.updateName} />
+                <input id="age-input" onChange={this.updateAge} />
+                <button onClick={this.alertText}>Go to Bar</button>
             </div>
         );
     }
